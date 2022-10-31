5ae15b6dca821ab6ce1e588b73e62907b4ff4f75
Ebrahim Byagowi <ebrahim@gnu.org>
2022-10-31 21:19:04 +0330

    Download ANTLR_JAR on the place

--- init.sh
+++ init.sh
@@ -3,21 +3,22 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-ANTLR=antlr4
-if [[ "$OSTYPE" == "darwin"* ]]; then ANTLR=antlr; fi # it's name in brew is without the version number
-PYTHON="/usr/bin/env python3"
+[ -x "$(command -v java)" ] || (echo "Please install Java from your package manager" && exit 1)
 
-[ -x "$(command -v $ANTLR)" ] || (echo "Please install antlr from your package manager" && exit 1)
-
-[ -d mediawiki ] || git clone https://github.com/wikimedia/mediawiki --depth=1
+ANTLR_JAR=antlr-4.11.1-complete.jar
+[ -f $ANTLR_JAR ] || wget https://www.antlr.org/download/$ANTLR_JAR
 
 [ -f PhpLexerBase.py ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/Python3/PhpLexerBase.py
 [ -f transformGrammar.py ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/Python3/transformGrammar.py
 [ -f PhpLexer.g4 ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/PhpLexer.g4
 [ -f PhpParser.g4 ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/PhpParser.g4
 
+java -jar $ANTLR_JAR -Dlanguage=Python3 PhpLexer.g4
+java -jar $ANTLR_JAR -Dlanguage=Python3 PhpParser.g4
+
+PYTHON="/usr/bin/env python3"
 $PYTHON -m pip install antlr4-python3-runtime
 $PYTHON transformGrammar.py
-$ANTLR -Dlanguage=Python3 PhpLexer.g4
-$ANTLR -Dlanguage=Python3 PhpParser.g4
+
+[ -d mediawiki ] || git clone https://github.com/wikimedia/mediawiki --depth=1
 $PYTHON main.py mediawiki/includes/changetags/ChangeTags.php
