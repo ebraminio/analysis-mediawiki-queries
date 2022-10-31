26f6d713558167c98f6f195a0e77bbc335d61d87
Ebrahim Byagowi <ebrahim@gnu.org>
2022-10-31 22:55:33 +0330

    Simplify using antlr4-tools

--- init.sh
+++ init.sh
@@ -1,22 +1,16 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o pipefail
-set -o nounset
-
-[ -x "$(command -v java)" ] || (echo "Please install Java from your package manager" && exit 1)
-
-ANTLR_JAR=antlr-4.11.1-complete.jar
-[ -f $ANTLR_JAR ] || wget https://www.antlr.org/download/$ANTLR_JAR
+#!/usr/bin/env bash -euo pipefail
 
 [ -f PhpLexerBase.py ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/Python3/PhpLexerBase.py
 [ -f transformGrammar.py ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/Python3/transformGrammar.py
 [ -f PhpLexer.g4 ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/PhpLexer.g4
 [ -f PhpParser.g4 ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/PhpParser.g4
 
-java -jar $ANTLR_JAR -Dlanguage=Python3 PhpLexer.g4
-java -jar $ANTLR_JAR -Dlanguage=Python3 PhpParser.g4
-
 PYTHON="/usr/bin/env python3"
+$PYTHON -m pip install antlr4-tools
+
+antlr4 -Dlanguage=Python3 PhpLexer.g4
+antlr4 -Dlanguage=Python3 PhpParser.g4
+
 $PYTHON -m pip install antlr4-python3-runtime
 $PYTHON transformGrammar.py
 
