d60b6da543197c3a44df1ea7ea40704d6d18ebd8
Ebrahim Byagowi <ebrahim@gnu.org>
2022-10-31 22:57:38 +0330

    Simplify init script even more

--- init.sh
+++ init.sh
@@ -5,14 +5,13 @@
 [ -f PhpLexer.g4 ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/PhpLexer.g4
 [ -f PhpParser.g4 ] || wget https://raw.githubusercontent.com/antlr/grammars-v4/master/php/PhpParser.g4
 
-PYTHON="/usr/bin/env python3"
-$PYTHON -m pip install antlr4-tools
+pip3 install antlr4-tools
 
 antlr4 -Dlanguage=Python3 PhpLexer.g4
 antlr4 -Dlanguage=Python3 PhpParser.g4
 
-$PYTHON -m pip install antlr4-python3-runtime
-$PYTHON transformGrammar.py
+pip3 install antlr4-python3-runtime
+python3 transformGrammar.py
 
 [ -d mediawiki ] || git clone https://github.com/wikimedia/mediawiki --depth=1
-$PYTHON main.py mediawiki/includes/changetags/ChangeTags.php
+python3 main.py mediawiki/includes/changetags/ChangeTags.php
